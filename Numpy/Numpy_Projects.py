318dfabbe50054a8ea886044168c86c701627a33
Sanidhya14321 <sanidhya14321@gmail.com>
2025-07-07 04:28:14 +0530

    projects on numpy

---
+++ Numpy/Numpy_Projects.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+# arr = np.array([1,2,3,4,5,6,7,8,9])
+# reshaped=arr.reshape(3, 3)
+# print("Original Array:\n",arr)
+# print("Reshaped Array:\n",reshaped)
+
+# arr=np.array([1,2,3])
+# expanded=arr[:,np.newaxis]
+# print("Original Array:\n", arr)
+# print("Expanded Array:\n", expanded)
+
+# arr=np.array([4,16,25,36,49,64,81])
+# sqrt_arr=np.sqrt(arr)
+# print("Original Array:\n", arr)
+# print("Square Root Array:\n", sqrt_arr)
+
+# 4x4 matrix and calculate the sum of its rows and columns
+# arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
+# row_sum = np.sum(arr, axis=1)
+# col_sum = np.sum(arr, axis=0)
+# print("Original Array:\n", arr)
+# print("Sum of Rows:\n", row_sum)
+# print("Sum of Columns:\n", col_sum)
+
+# Program to normalize an array(scaling values to a range of 0 to 1)
+# arr= np.array([10, 20, 30, 40, 50])
+# min_val = np.min(arr)
+# max_val = np.max(arr)
+# normalized_arr = (arr - min_val) / (max_val - min_val)
+# print("Original Array:\n", arr)
+# print("Normalized Array:\n", normalized_arr)
+
+#genearate a random 3x3 matrix and calculate its minimum, maximum, and mean values
+# arr= np.random.rand(3, 3)
+# min_val = np.min(arr)
+# max_val = np.max(arr)
+# mean_val = np.mean(arr)
+# print("Random 3x3 Matrix:\n", arr)
+# print("Minimum Value:", min_val)
+# print("Maximum Value:", max_val)
+# print("Mean Value:", mean_val)
+
+# Program to create a 2D array and transpose it
+# arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# transposed_arr = arr.T
+# print("Original Array:\n", arr)
+# print("Transposed Array:\n", transposed_arr)
