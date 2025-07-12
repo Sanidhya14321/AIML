4b42fe23d53e2a4e466080a75d042e74b29d21ed
Sanidhya14321 <sanidhya14321@gmail.com>
2025-07-12 23:20:57 +0530

    group by in pandas

---
+++ Pandas/Grouping.py
@@ -0,0 +1,23 @@
+grouped = df.groupby('column_name')
+
+for name, group in grouped:
+    print(f"\nGroup: {name}")
+    print(group)
+    
+grouped_mean = grouped.mean()
+
+grouped_sum = grouped.sum()
+
+df.groupby("category_column")["numeric_column"].mean()
+df.groupby("category_column").agg({"numeric_column": ["mean", "sum", "min", "max"]})
+
+pivot = df.pivot_table(index='category_column', values='numeric_column', aggfunc='mean')
+
+def range_func(x):
+    return x.max() - x.min()
+
+df.groupby("category_column")["numeric_column"].agg(range_func)
+
+df.groupby("category_column").agg(
+    {"numeric_column": ["mean", "sum", "min", "max"],}
+)
